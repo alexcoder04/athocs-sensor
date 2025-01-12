5005338010676222ee8c8de47ca43a4e86d6a316
alexcoder04 <alexcoder04@protonmail.com>
2025-01-13 00:28:37 +0100

    little fixes to make it run

--- rpi/athocs-sensor-logger.py
+++ rpi/athocs-sensor-logger.py
@@ -11,9 +11,9 @@ bus = smbus2.SMBus(1)
 
 params = bme280.load_calibration_params(bus, address)
 
-with open(LOG_FILE, "a") as f:
-    while True:
-        data = bme280.sample(bus, address, params)
+while True:
+    data = bme280.sample(bus, address, params)
+    with open(LOG_FILE, "a") as f:
         f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')},{data.temperature:.2f},{data.humidity:.1f},{data.pressure:.0f}\n")
-        time.sleep(INTERVAL)
+    time.sleep(INTERVAL)
 
