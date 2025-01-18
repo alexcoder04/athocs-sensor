a355b2924d1b37948835d9a6f8b8de2bf1d17418
alexcoder04 <alexcoder04@protonmail.com>
2025-01-18 17:02:50 +0100

    use cron for scheduling on rpi

--- rpi/athocs-sensor.py
+++ rpi/athocs-sensor.py
@@ -1,34 +1,29 @@
 import smbus2
 import bme280
-import time
 import datetime
 import requests
 
 IP = "192.168.0.86"
 PORT = 3000
 STATION_ID = "RPIZ-ALEX"
-INTERVAL = 60 * 5
 
 address = 0x76
 bus = smbus2.SMBus(1)
 
 params = bme280.load_calibration_params(bus, address)
 
-while True:
-    data = bme280.sample(bus, address, params)
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+data = bme280.sample(bus, address, params)
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
-    try:
-        requests.post(f"http://{IP}:{PORT}/api/upload", json={
-            "timestamp": timestamp,
-            "temperature": round(data.temperature, 2),
-            "humidity": round(data.humidity, 2),
-            "pressure": round(data.pressure, 1),
-            "station": STATION_ID,
-            "battery": 100
-            })
-    except Exception:
-        print("failed to upload data")
-
-    time.sleep(INTERVAL)
+try:
+    requests.post(f"http://{IP}:{PORT}/api/upload", json={
+        "timestamp": timestamp,
+        "station": STATION_ID,
+        "temperature": round(data.temperature, 2),
+        "humidity": round(data.humidity, 2),
+        "pressure": round(data.pressure, 1),
+        "battery": 100
+        })
+except Exception:
+    print(f"{timestamp},{STATION_ID},{temperature:.2f},{humidity:.2f},{pressure:.1f},100")
 
