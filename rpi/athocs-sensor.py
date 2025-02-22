2c136bbddc668a249487e637ef7c80ccf0b341c2
alexcoder04 <alexcoder04@protonmail.com>
2025-02-22 10:08:46 +0100

    add battery monitoring in raspberry pi

--- rpi/athocs-sensor.py
+++ rpi/athocs-sensor.py
@@ -4,6 +4,8 @@ import os
 import requests
 import smbus2
 
+from ups import INA219
+
 IP = os.getenv("ATHOCS_IP")
 PORT = os.getenv("ATHOCS_PORT")
 STATION_ID = os.getenv("ATHOCS_STATION_ID")
@@ -13,7 +15,10 @@ bus = smbus2.SMBus(1)
 
 params = bme280.load_calibration_params(bus, address)
 
+ina219 = INA219(addr=0x43)
+
 data = bme280.sample(bus, address, params)
+battery = (ina219.getBusVoltage_V() - 3.3)/0.9*100
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
 try:
@@ -23,8 +28,8 @@ try:
         "temperature": round(data.temperature, 2),
         "humidity": round(data.humidity, 2),
         "pressure": round(data.pressure, 1),
-        "battery": 100
+        "battery": round(battery)
         })
 except Exception:
-    print(f"{timestamp},{STATION_ID},{data.temperature:.2f},{data.humidity:.2f},{data.pressure:.1f},100")
+    print(f"{timestamp},{STATION_ID},{data.temperature:.2f},{data.humidity:.2f},{data.pressure:.1f},{battery:d}")
 
